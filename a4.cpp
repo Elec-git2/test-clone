afd26707546cec1e485afcac35d76954058dacae
Elec-Phil <15013020080@163.com>
2018-09-26 20:04:46 +0800

    删除一些branch2里面的文件

--- a4.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	cout << "a4" << endl;
-	return 0;
-}
--- example1/ex2.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-printf("ex2\n");
-return 0;
-}
